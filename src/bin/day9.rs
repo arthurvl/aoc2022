84ead20cc1b873d6b0e770498f2eac5c26406563
Arthur van Leeuwen <arthur.vanleeuwen@nedap.com>
2022-12-09 11:00:59 +0100

    Solve day 9 part 2
    Nasty trickiness; missed the 'more movements are now possible' bit.
    Love the snake :)

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -1,38 +1,40 @@
 use std::fs;
 use std::collections::HashSet;
 
-fn update_tail(head: &(i32,i32), tail: &mut (i32,i32)) -> () {
+fn new_tail(head: &(i32,i32), tail: &(i32,i32)) -> (i32,i32) {
     let dx = head.0 - tail.0;
     let dy = head.1 - tail.1;
 
-    dbg!(head,tail.clone(),dx,dy);
+    let new_tail: (i32,i32); 
 
     if dx.abs() == 2 && dy.abs() == 0 {
-        tail.0 = tail.0 + dx.signum();
+        new_tail = (tail.0 + dx.signum(), tail.1);
+    } else if dx.abs() == 0 && dy.abs() == 2 {
+        new_tail = (tail.0, tail.1 + dy.signum());
+    } else if dx.abs() == 2 && dy.abs() == 1 {
+        new_tail = (tail.0 + dx.signum(),  head.1);
+    } else if dx.abs() == 1 && dy.abs() == 2 {
+        new_tail = (head.0,  tail.1 + dy.signum());
+    } else if dx.abs() == 2 && dy.abs() == 2 {
+        new_tail = (tail.0 + dx.signum(), tail.1 + dy.signum());
+    } else {
+        new_tail = (tail.0,tail.1);
     }
 
-    if dx.abs() == 0 && dy.abs() == 2 {
-        tail.1 = tail.1 + dy.signum();
-    }
+//    dbg!(tail,dx,dy,new_tail);
 
-    if dx.abs() == 2 && dy.abs() == 1 {
-        tail.0 = tail.0 + dx.signum();
-        tail.1 = head.1;
-    }
-
-    if dx.abs() == 1 && dy.abs() == 2 {
-        tail.0 = head.0;
-        tail.1 = tail.1 + dy.signum();
-    }
-
-    dbg!(tail);
+    new_tail
 }
 
-fn record_tail_positions(movements: &str) -> HashSet<(i32,i32)> {
+fn record_tail_positions(movements: &str, tailsize: usize) -> HashSet<(i32,i32)> {
     let movements = movements.lines();
 
     let mut head = (0,0);
-    let mut tail = (0,0);
+    let mut tails = Vec::new();
+    
+    for _ in 0..tailsize {
+        tails.push((0,0));
+    }
 
     let mut seen_positions = HashSet::new();
 
@@ -57,10 +59,19 @@ fn record_tail_positions(movements: &str) -> HashSet<(i32,i32)> {
             head.0 = head.0 + dx;
             head.1 = head.1 + dy;
 
-            update_tail(&head,&mut tail);
+            let new = new_tail(&head, &tails[0]);
+            tails[0] = new;
 
-            seen_positions.insert(tail);
-        }
+            for i in 1..tailsize {
+                let new_tail = new_tail(&tails[i-1], &tails[i]);
+
+                tails[i] = new_tail;
+            };
+
+            dbg!(head,tails.clone());
+
+            seen_positions.insert(tails[tailsize-1]);
+        };
 
     };
 
@@ -68,7 +79,13 @@ fn record_tail_positions(movements: &str) -> HashSet<(i32,i32)> {
 }
 
 fn calculate_tail_positions(movements: &str) -> usize {
-    let positions = record_tail_positions(movements);
+    let positions = record_tail_positions(movements,1);
+
+    positions.len()
+}
+
+fn calculate_rope_positions(movements: &str) -> usize {
+    let positions = record_tail_positions(movements,9);
 
     positions.len()
 }
@@ -78,7 +95,7 @@ fn main() {
         .expect("Unable to read day 9 input");
 
     println!("{:#?}", calculate_tail_positions(&input));
-//    println!("{:#?}", find_marker_with_size(&input,14));
+    println!("{:#?}", calculate_rope_positions(&input));
 }
 
 #[cfg(test)]
@@ -98,4 +115,28 @@ R 2");
 
 #[test]
 fn example_2() {
+    let result = calculate_rope_positions("R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2");
+
+    assert_eq!(result,1);
+}
+
+#[test]
+fn example_3() {
+    let result = calculate_rope_positions("R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20");
+
+    assert_eq!(result,36);
 }
