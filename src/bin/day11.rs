645f9f4a5e369ef2e0a8e5f6a44d6ce8111093c4
Arthur van Leeuwen <arthur.vanleeuwen@nedap.com>
2022-12-12 16:55:33 +0100

    Add skeletons for remaining days

---
+++ src/bin/day11.rs
@@ -0,0 +1,36 @@
+use std::fs;
+
+fn part1(input: &str) -> &str {
+    input
+}
+
+fn part2(input: &str) -> &str {
+    input
+}
+
+fn main() {
+    let input= fs::read_to_string("/Users/arthur.vanleeuwen/scratch/aoc2022/input/day11/input")
+        .expect("Unable to read day 11 input");
+
+    println!("{:#?}", part1(&input.clone()));
+    println!("{:#?}", part2(&input));
+}
+
+#[cfg(test)]
+#[test]
+fn example_1() {
+    let result = "";
+    assert_eq!(result,"");
+}
+
+#[test]
+fn example_2() {
+    let result = 1;
+    assert_eq!(result,13140);
+}
+
+#[test]
+fn example_3() {
+    let result = "";
+    assert_eq!(result,"")
+}
